f5529fb2a80f5192f83db1cbbd927de9c50c1d61
yangxiaolin252412 <54714936+yangxiaolin252412@users.noreply.github.com>
2019-10-03 14:20:19 +0800

    Add files via upload

---
+++ CCActionManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using baseCode;
+public class CCActionManager : SSActionManager, ISSActionCallback
+{
+    //public firstSceneController sceneCtrl;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+        //sceneCtrl = (firstSceneController)(Director.getInstance().CurrentScenceController);
+        //sceneCtrl.actionManager = this;
+    }
+
+
+    public void SSActionEvent(SSAction source, SSActionEventType events = SSActionEventType.Competeted,
+        int intParam = 0, string strParam = null, Object objectParam = null)
+    {
+
+    }
+
+}
